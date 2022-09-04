2c9ae51181255ddaa710cd18e28547ac2940ccd5
osni.antonio <osni.antonio@gmail.com>
2022-09-04 14:11:52 -0300

    01-github-explorer

---
+++ 01-github-explorer/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  mode: isDevelopment ? "development" : "production",
+  devtool: isDevelopment ? "eval-source-map" : "source-map",
+  entry: path.resolve(__dirname, "src", "index.jsx"),
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+
+  devServer: {
+    //contentBase: path.resolve(__dirname, "public"),
+    //hot: true,
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
